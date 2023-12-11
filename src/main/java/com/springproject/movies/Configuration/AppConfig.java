a25f1241deced8914f5b65b1b5e5c0131d25a7f6
Utkarsh <dhavaleutkarsh@gmail.com>
2023-12-12 00:07:44 +0530

    Added In DB authentication

--- src/main/java/com/springproject/movies/Configuration/AppConfig.java
+++ src/main/java/com/springproject/movies/Configuration/AppConfig.java
@@ -2,29 +2,27 @@ package com.springproject.movies.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class AppConfig {
+public class AppConfig {  
 
+/*
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
     UserDetails user = User.withUsername("utkarsh")
-      .password(passwordEncoder().encode("Hq@1ewrretrye"))
-      .roles("ADMIN")
-      .build();
+        .password(passwordEncoder().encode("Hq@1ewrretrye"))
+        .roles("ADMIN")
+        .build();
     return new InMemoryUserDetailsManager(user);
-}
+    }
+*/
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -39,7 +37,9 @@ public class AppConfig {
         .requestMatchers("/api/**").hasRole("ADMIN")
         .requestMatchers("/swagger-ui/**").hasRole("ADMIN")
         .anyRequest().authenticated()
-        ).httpBasic(Customizer.withDefaults());
+        ).httpBasic(Customizer.withDefaults())
+        .formLogin(Customizer.withDefaults());
         return http.build();
     }
+
 }
